73cfdeaa0deb4d9c444d99c48e8cf07e33fabd33
bigmosi <kinyeramo@gmail.com>
2024-11-10 12:11:06 +0300

    create app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ActivityIndicator } from 'react-native';
 import PersonalInfo from './components/PersonalInfo';
 import Styles from './components/Styles';
 import Chat from './components/Chat';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [username, setUsername] = useState("");
   const [image, setImage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const onPersonalInfoClosed = ( username: string, image: string ) => {
-     setUsername(username);
-     setImage(image);
+  const storageUserNameKey = "chatapp-username";
+  const storageImageKey = "chatapp-image";
+
+  useEffect(() => {
+    fetchPersonalData();
+  }, []);
+
+  const fetchPersonalData = async () => {
+    let fetchedUsername = await AsyncStorage.getItem(storageUserNameKey);
+    let userName = fetchedUsername == null ? "" : fetchedUsername;
+    let fetchedImage = await AsyncStorage.getItem(storageImageKey);
+    let image = fetchedImage == null ? "" : fetchedImage;
+    setUsername(userName);
+    setImage(image);
+    setIsLoading(false);
+  };
+
+  const onPersonalInfoClosed = async (name: string, image: string) => {
+    setUsername(name);
+    await AsyncStorage.setItem(storageUserNameKey, name);
+    setImage(image);
+    await AsyncStorage.setItem(storageImageKey, image);
+  };
+
+  if (isLoading) {
+    return (
+      <View style={Styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading...</Text>
+      </View>
+    );
   }
 
-  let activeComponent = username != "" ? (
+  let activeComponent = username !== "" ? (
     <Chat username={username} image={image} />
   ) : (
-    <PersonalInfo />
-  )
+    <PersonalInfo onClosed={onPersonalInfoClosed} />
+  );
 
   return (
     <SafeAreaView style={Styles.container}>
-      { activeComponent }
+      {activeComponent}
       <StatusBar style="auto" />
     </SafeAreaView>
   );
